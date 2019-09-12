c24f66fa9340dfb78629feedc311be1c25c2923a
masha <maha_kors@ukr.net>
2019-09-12 12:31:57 +0300

    Add GET request to receive time in JSON format from api

--- server.go
+++ server.go
@@ -1,19 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"time"
+  "log"
+  "net/http"
+  "fmt"
+  "encoding/json"
 )
 
 func main() {
-	http.HandleFunc("/", handler) // each request calls handler
-	log.Fatal(http.ListenAndServe("localhost:8795", nil))
+  http.HandleFunc("/time", MakeRequest) // each request calls handler
+  log.Fatal(http.ListenAndServe("localhost:8795", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  currentTime := time.Now()
-  fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-  fmt.Fprintf(w, "Current Time in String: %q\n", currentTime.String())
+func MakeRequest(w http.ResponseWriter, r *http.Request) {
+  resp, err := http.Get("http://worldtimeapi.org/api/ip")
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  var result map[string]interface{}
+
+  json.NewDecoder(resp.Body).Decode(&result)
+
+  fmt.Fprintf(w, "Current time: %q\n", result["datetime"])
 }
