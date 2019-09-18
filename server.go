f6451af986e727d0be6ef1f2a7f578ceaba689c1
Denis Kalytovskyi <dkalitovskyi@gmail.com>
2019-09-18 12:32:18 +0300

    Format date to JSON

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,11 +9,22 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handler) // each request calls handler
+	http.HandleFunc("/time", handler) // each request calls handler
 	log.Fatal(http.ListenAndServe("localhost:8795", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  currentTime := time.Now()
-  fmt.Fprintf(w, currentTime.String())
+	currentTime := time.Now().Format(time.RFC3339)
+
+	type Message struct {
+		Time string `json:"time"`
+	}
+
+	b, err := json.Marshal(Message{Time: currentTime})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Fprintf(w, "%s", b)
 }
